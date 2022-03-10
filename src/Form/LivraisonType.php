4e1d9514683fec817b0d5a99204cc43bf2b6c741
NicolasMb-eng <80619223+NicolasMb-eng@users.noreply.github.com>
2022-03-10 14:55:50 +0100

    Add files via upload

---
+++ src/Form/LivraisonType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Livraison;
+use App\Entity\Livreur;
+use App\Entity\Order;
+use App\Entity\Paiement;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class LivraisonType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('livreur', EntityType::class, [
+                'class' => Livreur::class,
+                'choice_label' => 'nom',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Livraison::class,
+        ]);
+    }
+}
